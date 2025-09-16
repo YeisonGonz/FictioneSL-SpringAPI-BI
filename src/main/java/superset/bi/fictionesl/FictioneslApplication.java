3c20808c50b8e2e1c76a713a8cd7e400c927f175
YeisonGonz <yeisonrascado@gmail.com>
2025-09-16 05:55:44 +0200

    Start project

---
+++ src/main/java/superset/bi/fictionesl/FictioneslApplication.java
@@ -0,0 +1,13 @@
+package superset.bi.fictionesl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictioneslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FictioneslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/superset/bi/fictionesl/FictioneslApplicationTests.java
@@ -0,0 +1,13 @@
+package superset.bi.fictionesl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FictioneslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
