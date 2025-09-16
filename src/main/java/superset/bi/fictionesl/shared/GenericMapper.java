c1b6a8772dffdcb97ebb16d14f7b02f63ea27b9e
YeisonGonz <yeisonrascado@gmail.com>
2025-09-16 06:56:17 +0200

    Added a GenericMapper to implement in entity

---
+++ src/main/java/superset/bi/fictionesl/shared/GenericMapper.java
@@ -0,0 +1,18 @@
+package superset.bi.fictionesl.shared;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public interface GenericMapper<E, D> {
+    E toEntity(D dto);
+
+    D toDto(E entity);
+
+    default List<E> toEntityList(List<D> dtoList) {
+        return dtoList.stream().map(this::toEntity).collect(Collectors.toList());
+    }
+
+    default List<D> toDtoList(List<E> entityList) {
+        return entityList.stream().map(this::toDto).collect(Collectors.toList());
+    }
+}
