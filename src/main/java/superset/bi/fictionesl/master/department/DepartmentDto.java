1e0425304d5c69ae04752866ba863f06fdaa4891
YeisonGonz <yeisonrascado@gmail.com>
2025-09-16 19:14:28 +0200

    Fix Department Dto

--- src/main/java/superset/bi/fictionesl/master/department/DepartmentDto.java
+++ src/main/java/superset/bi/fictionesl/master/department/DepartmentDto.java
@@ -4,4 +4,15 @@ import superset.bi.fictionesl.master.employee.EmployeeDto;
 
 import java.util.List;
 
-public record DepartmentDto(String name, String location,  List<EmployeeDto> employees ) { }
+public record DepartmentDto(
+        String name,
+        String location,
+        List<EmployeeDto> employees
+) {
+    public DepartmentDto {
+        if (employees == null) {
+            employees = List.of(); // o new ArrayList<>()
+        }
+    }
+}
+
--- src/main/java/superset/bi/fictionesl/master/department/DepartmentMapper.java
+++ src/main/java/superset/bi/fictionesl/master/department/DepartmentMapper.java
@@ -1,10 +1,15 @@
 package superset.bi.fictionesl.master.department;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValueMappingStrategy;
 import superset.bi.fictionesl.master.employee.EmployeeMapper;
 import superset.bi.fictionesl.shared.GenericMapper;
 
 // Le indicas que tiene que usar este mapper en el
 // DTO de Department para que pase correctamente de Employee a EmployeeDto
-@Mapper(componentModel = "spring", uses = EmployeeMapper.class)
+@Mapper(
+        componentModel = "spring",
+        uses = EmployeeMapper.class,
+        nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT
+)
 public interface DepartmentMapper extends GenericMapper<Department, DepartmentDto> { }
